a2286394f89a7def01f9710f4316887fe6b4f907
Chris Van <cvan@mozilla.com>
2013-05-09 16:01:48 -0700

    update

--- carrierinfo/detect.js
+++ carrierinfo/detect.js
@@ -1,5 +1,5 @@
 (function() {
-  var conn = navigator.mozMobileConnection;
+  var conn = navigator.mozMobileConnection || {};
   console.log('navigator.mozMobileConnection:', JSON.stringify(conn));
   if (!conn) {
     document.querySelector('dl').innerHTML = '<dt>Unavailable</dt><dd></dd>';
@@ -7,14 +7,15 @@
   }
   var output = [];
   output.push('<dt>lastKnownNetwork</dt><dd>' + conn.lastKnownNetwork + '</dd>');
+  output.push('<dt>lastKnownHomeNetwork</dt><dd>' + conn.lastKnownHomeNetwork + '</dd>');
   output.push('<dt>cardState</dt><dd>' + conn.cardState + '</dd>');
   output.push('<dt>data</dt><dd>' + conn.data + '</dd>');
-  output.push('<dt>iccInfo.mcc</dt><dd>' + conn.iccInfo.mcc + '</dd>');
-  output.push('<dt>iccInfo.mnc</dt><dd>' + conn.iccInfo.mnc + '</dd>');
-  output.push('<dt>iccInfo.iccid</dt><dd>' + conn.iccInfo.iccid + '</dd>');
-  output.push('<dt>voice.cell.gsmLocationAreaCode</dt><dd>' + conn.voice.cell.gsmLocationAreaCode + '</dd>');
-  output.push('<dt>voice.cell.gsmCellId</dt><dd>' + conn.voice.cell.gsmCellId + '</dd>');
-  output.push('<dt>voice.connected</dt><dd>' + conn.voice.connected + '</dd>');
-  output.push('<dt>voice.emergencyCallsOnly</dt><dd>' + conn.voice.emergencyCallsOnly + '</dd>');
+  output.push('<dt>iccInfo.mcc</dt><dd>' + (conn.iccInfo || {}).mcc + '</dd>');
+  output.push('<dt>iccInfo.mnc</dt><dd>' + (conn.iccInfo || {}).mnc + '</dd>');
+  output.push('<dt>iccInfo.iccid</dt><dd>' + (conn.iccInfo || {}).iccid + '</dd>');
+  output.push('<dt>voice.cell.gsmLocationAreaCode</dt><dd>' + ((conn.voice || {}).cell || {}).gsmLocationAreaCode + '</dd>');
+  output.push('<dt>voice.cell.gsmCellId</dt><dd>' + ((conn.voice || {}).cell || {}).gsmCellId + '</dd>');
+  output.push('<dt>voice.connected</dt><dd>' + (conn.voice || {}).connected + '</dd>');
+  output.push('<dt>voice.emergencyCallsOnly</dt><dd>' + (conn.voice || {}).emergencyCallsOnly + '</dd>');
   document.querySelector('dl').innerHTML = output;
 })();
